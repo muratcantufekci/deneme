b1b50b811770d99885fb8a2ff454e445ae18ef57
Muratcan Tüfekçi <1600002065@STU.IKU.EDU.TR>
2021-02-17 22:44:50 +0300

    deneme

---
+++ Metotlar/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Metotlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Product urun1 = new Product();
+            urun1.Id = 1;
+            urun1.Adi = "Elma";
+            urun1.Fiyatı = 15;
+            urun1.Aciklama = "Amasya elması";
+
+            Product urun2 = new Product();
+            urun2.Adi = "Karpuz";
+            urun2.Fiyatı = 80;
+            urun2.Aciklama = "Diyarbakır karpuzu";
+
+
+            Product[] urunler = new Product[] { urun1, urun2 };
+
+            foreach (Product urun in urunler)
+            {
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine(urun.Fiyatı);
+                Console.WriteLine(urun.Aciklama);
+
+            }
+
+            Console.WriteLine("----------------Metotlar------");
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yeşil armut", 12,10);
+
+            Console.ReadKey(); 
+        }
+    }
+}
