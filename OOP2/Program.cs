f86474ded9c08b3f58fc085f24021e5a3596c2de
Muratcan Tüfekçi <1600002065@STU.IKU.EDU.TR>
2021-02-22 19:52:53 +0300

    5.Ders sonu

---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Muratcan";
+            musteri1.Soyadi = "Tüfekçi";
+            musteri1.Tc = "1223442112";
+
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "2131";
+            musteri2.SirketAdi = "Kodlama.io";
+            musteri2.VergiNo = "2131313";
+
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Add(musteri1);
+            musteriManager.Add(musteri2);
+        }
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();            
+            IKrediManager tasitKrediManager = new TasitKrediManager();            
+            IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService databaseLoggerServise = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager, fileLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() { ıhtiyacKrediManager,tasitKrediManager};
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+            
+            Console.ReadKey();
+        }
+    }
+}
